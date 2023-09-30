8e60f74149cb73f8a8414ceae2582e4bb8dc9fe2
Ruben Barros <atosc08@gmail.com>
2023-09-30 21:36:01 +0200

    fix: add to kv

--- server/api/likes.post.ts
+++ server/api/likes.post.ts
@@ -6,7 +6,7 @@ export default defineEventHandler(async (event) => {
   const body = await readBody<Body>(event)
   const { cloudflare } = event.context
   const like = body.increment ? 1 : -1
-  const currentLikes = (await cloudflare.env.MIAOUUU.get('likes')) ?? 0
+  const currentLikes = +(await cloudflare.env.MIAOUUU.get('likes')) ?? 0
   const sum = currentLikes + like
   await cloudflare.env.MIAOUUU.put('likes', sum > 0 ? sum : 0)
   return {
